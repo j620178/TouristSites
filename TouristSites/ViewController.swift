6b46ee228c9262977d042b95b11243fb2c3b1581
littlema <j620178@gmail.com>
2019-10-31 07:55:53 +0800

    init commit

---
+++ TouristSites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouristSites
+//
+//  Created by littlema on 2019/10/31.
+//  Copyright © 2019 littema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
