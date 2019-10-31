52630a1c336c3df19e7780ea075f36592f52e921
littlema <j620178@gmail.com>
2019-10-31 08:08:03 +0800

    Establish MainViewController

---
+++ TouristSites/Helper/UIColor+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extension.swift
+//  TouristSites
+//
+//  Created by littlema on 2019/10/30.
+//  Copyright © 2019 littema. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundGray: UIColor {
+        return UIColor(red: 246 / 255, green: 246 / 255, blue: 246 / 255, alpha: 1)
+    }
+}
---
+++ TouristSites/View/MainViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  TouristSites
+//
+//  Created by littlema on 2019/10/31.
+//  Copyright © 2019 littema. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .backgroundGray
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        return tableView
+    }()
+    
+    lazy var indicatorView: UIActivityIndicatorView = {
+        let indicatorView = UIActivityIndicatorView(style: .medium)
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        indicatorView.hidesWhenStopped = true
+        self.view.addSubview(indicatorView)
+        NSLayoutConstraint.activate([
+            indicatorView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            indicatorView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+        return indicatorView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+
+    }
+    
+    func setupView() {
+        title = "台北市熱門景點"
+        view.backgroundColor = .backgroundGray
+    }
+
+}
+
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+extension MainViewController: UITableViewDelegate {}
+
